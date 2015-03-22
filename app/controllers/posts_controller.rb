ec5fb1a9a368c2c27992ec69eb35951921a836c4
Michael Do <mdo3739@gmail.com>
2015-03-22 18:38:52 -0500

    Finished associations checkpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,6 @@
 class PostsController < ApplicationController
   def index
   	@posts = Post.all
-
-
   end
 
   def show
@@ -14,7 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
