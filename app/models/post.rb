30bbfd5e51195c61bb8d334f3a8bf9913c98bc1e
Michael Do <mdo3739@gmail.com>
2015-03-30 00:10:58 -0500

    finished uploading images assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,11 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :topic
 	default_scope {order('created_at DESC')}
+	mount_uploader :pic, PostPicUploader
 
 	validates :title, length: {minimum: 5}, presence: true
 	validates :body, length: {minimum: 20}, presence: true
 	validates :topic, presence: true
 	validates :user, presence: true
+	validates :pic, presence: true
 end
