3287c64c5432ef394191176e1c779dc8629e3ac0
Michael Do <mdo3739@gmail.com>
2015-03-30 17:18:05 -0500

    Halfway done with paginate assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :topic
 	default_scope {order('created_at DESC')}
+	require 'paginate'
 
 	validates :title, length: {minimum: 5}, presence: true
 	validates :body, length: {minimum: 20}, presence: true
