8862ff968e1d43f91f5d74d804b63b84c92c77ad
Michael Do <mdo3739@gmail.com>
2015-03-30 19:05:58 -0500

    Finished comments checkpoint

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+	def create
+		@topic = Topic.find(params[:topic_id])
+		@post = Post.find(params[:post_id])
+		@comment = current_user.comments.build(comment_params)
+		@comment.post = @post
+		if @comment.save
+			flash[:notice] = "Your comment was posted"
+			redirect_to [@topic, @post]
+		else
+			flash[:error] = "There was an error saving your comment.  Please try again."
+			redirect_to [@topic, @post]
+		end
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:body)
+	end
+end
