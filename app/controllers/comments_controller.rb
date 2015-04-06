ad069addf2a32c5fc815500786a8990fbe99112c
Michael Do <mdo3739@gmail.com>
2015-04-06 01:48:23 -0500

    Finished destroy assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 	def create
-		@topic = Topic.find(params[:topic_id])
 		@post = Post.find(params[:post_id])
+		@topic = @post.topic
 		@comment = current_user.comments.build(comment_params)
 		@comment.post = @post
 		authorize @comment
@@ -15,8 +15,8 @@ class CommentsController < ApplicationController
 	end
 
 	def destroy
-		@topic = Topic.find(params[:topic_id])
 		@post = Post.find(params[:post_id])
+		@topic = @post.topic
 		@comment = Comment.find(params[:id])
 		authorize @comment
 		if @comment.destroy
