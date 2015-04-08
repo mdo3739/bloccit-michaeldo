599cb18c4f587cf2e52290b672b88316f92cf91a
Michael Do <mdo3739@gmail.com>
2015-04-08 02:45:27 -0500

    Finished voting checkpoint

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :topic
 	has_many :votes, dependent: :destroy
-	default_scope {order('created_at DESC')}
+	default_scope {order('rank DESC')}
 	scope :ordered_by_title, -> {reorder(:title)}
 
 	validates :title, length: {minimum: 5}, presence: true
@@ -21,5 +21,12 @@ class Post < ActiveRecord::Base
 
 	def points
 		up_votes - down_votes
+	end
+
+	def update_rank
+		age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+		new_rank = points + age_in_days
+
+		update_attribute(:rank, new_rank)
 	end
 end
