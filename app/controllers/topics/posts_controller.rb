95e4ecba172895877c8be65a92c0499bfe6fd186
Michael Do <mdo3739@gmail.com>
2015-04-18 00:41:09 -0500

    Finished popular posts checkpoint

---
+++ app/controllers/topics/posts_controller.rb
@@ -0,0 +1,73 @@
+class Topics::PostsController < ApplicationController
+
+  def show
+  	@post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
+
+    @comment = Comment.new
+    @comments = @post.comments.all
+    authorize @comment
+    authorize @topic
+
+    @summary = @post.summary
+    authorize @post
+  end
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new
+    authorize @post
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    @topic = Topic.find(params[:topic_id])
+    @post.topic = @topic
+    authorize @post
+    if @post.save_with_initial_vote
+      flash[:notice] = "Post was saved."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
+    authorize @post
+  end
+
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Post was updated."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+    if @post.destroy
+      flash[:notice] = "Post was deleted."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error deleting your post. Please try again."
+      redirect_to :show
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :pic)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :users, only: [:update, :show]
+  resources :users, only: [:update, :show, :index]
   get 'welcome/index'
   get 'about', to: "welcome#about"
   get 'welcome/contact'
   resources :advertisements
   resources :topics do
-  	resources :posts, except: [:index] 
+  	resources :posts, except: [:index], controller: 'topics/posts'
   end
 
-  resources :posts, only: [] do
+  resources :posts, only: [:index] do
       resources :comments, only: [:create, :destroy]
       resources :summaries, except: [:index]
       resources :favorites, only: [:create, :destroy]
