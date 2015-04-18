188b86a3d07ce9148db9171efe87bcc3357f6202
Michael Do <mdo3739@gmail.com>
2015-04-17 22:43:40 -0500

    Finished public profiles assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 describe "Visiting profiles" do
 	include TestFactories
+	include Warden::Test::Helpers
+	Warden.test_mode!
 
 	before do
 		@user = authenticated_user
 		@post = associated_post(user: @user)
-		@comment = Comment.new(user: @user, body: "A comment")
+		@comment = Comment.new(user: @user, body: "A comment", post: @post)
 		allow(@comment).to receive(:send_favorite_emails)
 		@comment.save!
 	end
@@ -19,6 +21,14 @@ describe "Visiting profiles" do
 			expect(page).to have_content(@user.name)
 			expect(page).to have_content(@post.title)
 			expect(page).to have_content(@comment.body)
+		end
+	end
+
+	describe "when signed in" do
+		it "shows own profile" do
+			login_as(@user, scope: :user)
+			visit user_path(@user)
+			expect(current_path).to eq(user_path(@user))
 		end
 	end
 end
